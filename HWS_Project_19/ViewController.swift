638ca7460adb76cd8f88fd1dbca07d6dd8aab93a
Cory Tepper <corytepper@Corys-MacBook-Pro.local>
2021-01-04 13:36:01 -0800

    basic project working with NSExtension, Javascript injection extension for Safari

---
+++ HWS_Project_19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HWS_Project_19
+//
+//  Created by Cory Tepper on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
